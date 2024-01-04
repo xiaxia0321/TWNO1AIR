1cf3f264cd2c28126d3155e983e4117662554e92
xiaxia0321 <zmzmzmuuzm@gmail.com>
2024-01-04 15:11:35 +0800

    失敗登入

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,15 +9,22 @@ export default {
 <template>
   <div class="big">
     <div class="mid">
+
+      <h1>體驗完整功能</h1>
+      <h3>追蹤價格，快速比價，規劃行程</h3>
+
       <div class="in1">
-        <label for="">帳號</label>
+        <p>帳號</p>
         <input type="text" class="input" />
       </div>
 
       <div class="in2">
-        <label for="">密碼</label>
+        <p>密碼</p>
         <input type="password" class="input" name="" id="" />
       </div>
+
+      <button type="button" class="in3">登入</button>
+
     </div>
   </div>
 </template>
@@ -30,13 +37,26 @@ export default {
   margin: 0 auto;
   .mid {
     width: 40vw;
-    height: 60vh;
+    height: 80vh;
     border: 1px solid black;
     margin: 0 auto;
-    .in1{
-      margin: 10vh  auto;
+    p{
+      margin: 0;
+      margin-right: 23vw;
+
     }
-    .input {
+    .in1{ //帳號輸入框
+      margin-top: 5%;
+      margin-bottom: 5%;
+    }
+    .in2{ //密碼輸入框
+      margin-bottom: 8%;
+    }
+    .in3{ //登入按鈕
+      width: 25vw;
+      height: 6vh;
+    }
+    .input { 
       width: 25vw;
       height: 5vh;
     }
