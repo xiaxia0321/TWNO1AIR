48eb5df1cd6405095908f74cdc0db13dc6bf986b
xiaxia0321 <zmzmzmuuzm@gmail.com>
2024-02-01 13:48:29 +0800

    2/1

--- src/views/ProductDetailed.vue
+++ src/views/ProductDetailed.vue
@@ -28,7 +28,7 @@ export default {
   },
   methods: {
     test() {
-      console.log(this.userDate.uuu[0]);
+      console.log(this.depatureTime);
 
     },
     loginDD() {
@@ -47,7 +47,6 @@ export default {
         // 登录成功后的处理逻辑
         console.log(res);
         console.log(res.data.rtnCode);
-        localStorage.setItem('authToken', res.data.authToken);
 
       }).catch(error => {
         // 登录失败后的处理逻辑
@@ -74,17 +73,31 @@ export default {
     },
 
     Ordergogo() {
-      const authToken = localStorage.getItem('authToken');
-      if (authToken) {
+
+      console.log("測試:", {
+    oneway: this.planeSearchCheack.ccc.isOneway,
+    numberOfPeople: this.Order.getAddPeople[0].enteredPeople,
+    departureDate: this.planeSearchCheack.ccc.departureDate,
+    arrivalDate: this.planeSearchCheack.ccc.arriveDate,
+    departureLocation: this.planeSearchCheack.ccc.departureLocation,
+    arrivalLocation: this.planeSearchCheack.ccc.arrivalLocation,
+    classType: this.planeSearchCheack.ccc.classType,
+    price: this.Order.getPrice * this.Order.getAddPeople[0].enteredPeople,
+    account: this.userDate.uuu[0].account,
+    depatureTime: this.planeSearchCheack.ccc.depatureTime,
+    arriveTime: this.planeSearchCheack.ccc.arriveTime,
+    addPeople: this.Order.getAddPeople,
+    seat: this.planeSearchCheack.seat,
+    airplainId: this.Order.getAddPeople[0].enteredPeople,
+  });
         axios({
           url: 'http://localhost:8080/order/create',
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${authToken}`,
           },
           data: {
-            is_oneway: this.planeSearchCheack.ccc.isOneway, //單程
+            oneway: this.planeSearchCheack.ccc.isOneway, //單程
             numberOfPeople: this.Order.getAddPeople[0].enteredPeople, //人數
             departureDate: this.planeSearchCheack.ccc.departureDate, //出發日期
             arrivalDate: this.planeSearchCheack.ccc.arriveDate, //抵達日期
@@ -92,45 +105,36 @@ export default {
             arrivalLocation: this.planeSearchCheack.ccc.arrivalLocation, //抵達地
             classType: this.planeSearchCheack.ccc.classType, //艙等
             price: this.Order.getPrice * this.Order.getAddPeople[0].enteredPeople, //價錢
-            account: this.userDate.uuu[0].name, //帳戶名稱有問題
+            account: this.userDate.uuu[0].account, //帳戶名稱有問題
             depatureTime: this.planeSearchCheack.ccc.depatureTime, //出發時間
             arriveTime: this.planeSearchCheack.ccc.arriveTime, //抵達時間
-            add_people: this.Order.getAddPeople, //打包人數資料
+            addPeople: JSON.stringify(this.Order.getAddPeople),
             seat: this.planeSearchCheack.seat, //座位編號
+            airplainId: this.Order.getAddPeople[0].enteredPeople,
           },
         })
           .then(res => {
-            if (res.status === 200 && res.data.code === 200) {
-              console.log("Order created successfully");
-            } else if (res.status === 200 && res.data.code === 400) {
-              console.error("Order creation failed:", res.data.message);
-              // 在这里处理用户未登录的情况，例如跳转到登录页面
-              // this.$router.push("/login");
-            } else {
-              console.error("Unexpected response:", res);
-            }
+            console.log("成功");
+            this.$router.push("/User");
           })
-      } else {
-        console.error("No authToken available. Please log in first.");
-      }
-      console.log(this.Order);
-      this.Order = {
-        is_oneway: "",
-        numberOfPeople: 0,
-        departureDate: "",
-        arrivalDate: "",
-        departureLocation: "",
-        arrivalLocation: "",
-        classType: "",
-        price: false,
-        account: "",
-        depatureTime: "",
-        arriveTime: "",
-        add_people: "",
+      // console.log(this.Order);
+      // this.Order = {
+      //   is_oneway: "",
+      //   numberOfPeople: 0,
+      //   departureDate: "",
+      //   arrivalDate: "",
+      //   departureLocation: "",
+      //   arrivalLocation: "",
+      //   classType: "",
+      //   price: false,
+      //   account: "",
+      //   depatureTime: "",
+      //   arriveTime: "",
+      //   add_people: "",
 
-      }
+      // }
     },
-    // this.$router.push('/Backstage/BackPlane')
+   
 
 
     calculateDuration(depatureTime, arriveTime) {
@@ -171,8 +175,8 @@ export default {
       <h2>產品明細</h2>
       <h3>航班資訊</h3>
       <!-- <button @click="loginDD">登入</button>
-      <button @click="logoutDD">登出</button>
-      <button @click="test">測試</button> -->
+      <button @click="logoutDD">登出</button> -->
+      <!-- <button @click="test">測試</button> -->
     </div>
     <div class="mt1"><span>經濟艙</span></div>
     <div class="mid1">
@@ -249,7 +253,7 @@ export default {
   </div>
   <div class="bottom1">
     <span>TWD {{ this.Order.getPrice * this.Order.getAddPeople[0].enteredPeople }}</span>
-    <button @click="Ordergogo">前往付款</button>
+    <button @click="Ordergogo">送出訂單</button>
   </div>
 </template>
 
