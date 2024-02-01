6ede7c8c0fdc7d19f43d103afb04928315d7cd09
謝易樺 <a46644231@gmail.com>
2024-02-01 17:19:17 +0800

    建立檔案

---
+++ src/views/BackstageMemberInformation.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            userId: "",
+            account: "",
+            password: "",
+            email:"",
+            name:"",
+            phone:"",
+            age:"",
+            birthday:"",
+        };
+    }
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss"></style>
